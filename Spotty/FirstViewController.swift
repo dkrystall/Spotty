385da89282bff8b228d355376f2d83f39e95fe53
David Krystall <david.krystall@ontometrics.com>
2017-02-11 15:44:12 -0800

    Initial Commit

---
+++ Spotty/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Spotty
+//
+//  Created by David Krystall on 2/11/17.
+//  Copyright © 2017 David Krystall. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
