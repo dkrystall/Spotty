d45fb103ac12620d02023d71032dd80ecd63b7ba
David Krystall <david.krystall@ontometrics.com>
2017-02-11 23:47:30 -0800

    Added logic to skip loading the account creation view if there is already an account in the database

--- Spotty/FirstViewController.swift
+++ Spotty/FirstViewController.swift
@@ -7,12 +7,47 @@
 //
 
 import UIKit
+import CoreData
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet var userName: UILabel!
+    var accountMade = false
     override func viewDidLoad() {
+        
         super.viewDidLoad()
+        
+        if let alreadyMade = UserDefaults.standard.value(forKey: "AccountMadeAlready") as? Bool {
+            accountMade = alreadyMade
+        }
+        
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Profile")
+        request.returnsObjectsAsFaults = false
+        
+        do{
+            let results = try context.fetch(request)
+            if results.count > 0 {
+                for result in results as! [NSManagedObject] {
+                    let fetchedUserName = result.value(forKey: "name") as? String
+                    userName.text = fetchedUserName
+                    print(userName.text)
+                }
+            }
+        } catch{
+            print("fetch failed")
+        }
+        
+        self.navigationController?.hidesBarsOnTap = true
         // Do any additional setup after loading the view, typically from a nib.
+        
+        if !accountMade {
+            let alert = UIAlertController.init(title: "Account Made Successfully", message: "Don't hurt yourself.", preferredStyle: .alert)
+            let ok = UIAlertAction.init(title: "Yay!", style: UIAlertActionStyle.default, handler: nil)
+            alert.addAction(ok)
+            self.present(alert, animated: true, completion: nil)
+        }
     }
 
     override func didReceiveMemoryWarning() {
--- Spotty/onboardingTableViewController.swift
+++ Spotty/onboardingTableViewController.swift
@@ -22,30 +22,46 @@ class onboardingTableViewController: UITableViewController {
     @IBOutlet var squatTextField: UITextField!
     @IBOutlet var deadliftTextField: UITextField!
     @IBOutlet var rowTextField: UITextField!
-    
+    var accountAlreadyMade = false
     override func viewDidLoad() {
         super.viewDidLoad()
         print("setting up table")
-        appContext = appDelegate.persistentContainer.viewContext
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
-
+        
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        appContext = appDelegate.persistentContainer.viewContext
+        
+        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "Profile")
+        request.returnsObjectsAsFaults = false
+        do {
+            let results = try appContext?.fetch(request)
+            if (results?.count)! > 0 {
+                performSegue(withIdentifier: "mainSegue", sender: self)
+                print("Account already exists")
+                accountAlreadyMade = true
+                UserDefaults.standard.setValue(true, forKey: "AccountMadeAlready")
+            }
+        } catch {
+            print("erro")
+        }
+        
+    }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
     // MARK: - Table view data source
-
+    
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
         return 2
     }
-
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
         switch section{
@@ -66,69 +82,83 @@ class onboardingTableViewController: UITableViewController {
         //save context
         let sex = self.sexSegmentedControl.selectedSegmentIndex
         let experience:Int16 = Int16(self.experienceSegmentControl.selectedSegmentIndex)
-        do{
-            if let context = appContext{
-                if let newUser = NSEntityDescription.insertNewObject(forEntityName: "Profile", into: context) as? Profile{
-                    newUser.name = nameTextFIeld.text
-                    sex > 0 ? (newUser.sex=1) : (newUser.sex = 0)
-                    if let weight = Double(weightTextField.text!){
-                        newUser.weight = weight
-                        print("Weight set")
+        if !accountAlreadyMade{
+            do{
+                if let context = appContext{
+                    if let newUser = NSEntityDescription.insertNewObject(forEntityName: "Profile", into: context) as? Profile{
+                        newUser.name = nameTextFIeld.text
+                        newUser.setValue(newUser.name, forKey: "name")
+                        sex > 0 ? (newUser.sex=1) : (newUser.sex = 0)
+                        newUser.setValue(newUser.sex, forKey: "sex")
+                        if let weight = Double(weightTextField.text!){
+                            newUser.weight = weight
+                            newUser.setValue(newUser.weight, forKey: "weight")
+                            print("Weight set")
+                        }
+                        newUser.experience = experience
+                        newUser.setValue(experience, forKey: "experience")
+                        print("sex is: \(newUser.sex)")
+                        try context.save()
                     }
-                    newUser.experience = experience
-                    print("sex is: \(newUser.sex)")
-                    try context.save()
                 }
             }
+            catch {
+                print("Context Invalid")
+            }
         }
-        catch {
-            print("Context Invalid")
-        }
     }
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    @IBAction func nextPressed(_ sender: Any) {
+        performSegue(withIdentifier: "mainSegue", sender: self)
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+        //let nav = self.navigationController
+        self.navigationController!.viewControllers.removeAll()
     }
-    */
-
+    
+    /*
+     // Override to support conditional editing of the table view.
+     override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the specified item to be editable.
+     return true
+     }
+     */
+    
+    /*
+     // Override to support editing the table view.
+     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+     if editingStyle == .delete {
+     // Delete the row from the data source
+     tableView.deleteRows(at: [indexPath], with: .fade)
+     } else if editingStyle == .insert {
+     // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+     }
+     }
+     */
+    
+    /*
+     // Override to support rearranging the table view.
+     override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+     
+     }
+     */
+    
+    /*
+     // Override to support conditional rearranging of the table view.
+     override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+     // Return false if you do not want the item to be re-orderable.
+     return true
+     }
+     */
+    
+    /*
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destinationViewController.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
